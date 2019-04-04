3f4a04bc24347ebed18b9addcaf4360d2e6274f7
djs5435 <djs5435@IGMOH1370-18>
2019-04-04 14:22:23 -0400

    Remove physics cubes

--- C22_Physics/AppClass.cpp
+++ C22_Physics/AppClass.cpp
@@ -13,18 +13,6 @@ void Application::InitVariables(void)
 	m_pEntityMngr->AddEntity("Minecraft\\Steve.obj", "Steve");
 	m_pEntityMngr->UsePhysicsSolver();
 	
-	for (int i = 0; i < 100; i++)
-	{
-		m_pEntityMngr->AddEntity("Minecraft\\Cube.obj", "Cube_" + std::to_string(i));
-		vector3 v3Position = vector3(glm::sphericalRand(12.0f));
-		v3Position.y = 0.0f;
-		matrix4 m4Position = glm::translate(v3Position);
-		m_pEntityMngr->SetModelMatrix(m4Position * glm::scale(vector3(2.0f)));
-		m_pEntityMngr->UsePhysicsSolver();
-		//m_pEntityMngr->SetMass(2);
-
-		//m_pEntityMngr->SetMass(i+1);
-	}
 }
 void Application::Update(void)
 {
